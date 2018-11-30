34180ac363afd587902441b204a49a29e9e0ba7a
agustinasusanti <susantia525@gmail.com>
2018-11-30 15:14:05 +0700

    first commit

---
+++ latihan1.cpp
@@ -0,0 +1,17 @@
+#include<iostream>
+
+using namespace std;
+
+int main() {
+    int p, l, luas;
+
+	cout << "Masukkan panjang: ";
+	cin >> p;
+	cout << "Masukkan lebar: ";
+	cin >> l;
+
+	luas = p * l;
+
+	cout << "luas persegi panjang adalah: " << luas << endl ;
+	return 0;
+}
